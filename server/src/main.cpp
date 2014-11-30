087c4e6158721d93ce5955aabf0c62749a8ac18b
debas <etienne.debas@gmail.com>
2014-11-30 20:19:56 +0100

    add test thread bug

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
+#include <thread>
+#include "ThreadPool.hpp"
+
+void test()
+{
+  std::cout << "test thread" << std::endl;
+}
 
 int main(int ac, char *av[])
 {
-
+//  ThreadPool pool(5);
   std::cout << "Hello from server" << std::endl;
 
+  std::thread t(&test);
+  t.join();
   return 0;
 }
