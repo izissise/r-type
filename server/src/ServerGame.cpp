c151871067f3136f84d24c0c5cd71e73777f69c0
Hugues Morisset <morisset.hugues@gmail.com>
2014-12-19 19:42:27 +0100

    Cooreclty accepting new client on servergAME

--- server/src/ServerGame.cpp
+++ server/src/ServerGame.cpp
@@ -1,14 +1,14 @@
 #include "ServerGame.hpp"
 
-#include <chrono>
-#include <thread>
+#include <iostream>
 
 ServerGame::ServerGame(const ServerRoom& gameInfo, const std::string& listeningAddr)
   : _runGame(true),
     _net(Network::NetworkFactory::createNetwork()),
     _udpListener(Network::NetworkFactory::createListenSocket(listeningAddr, "", Network::ASocket::SockType::UDP, true))
 {
-  //_udpListener->setNewConnectionCallback();
+	std::cout << "New game on: " << _udpListener->getListeningIpAddr() << ":" << _udpListener->getListeningPort() << std::endl;
+  _udpListener->setNewConnectionCallback(std::bind(&ServerGame::joinGame, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
   _net->registerListener(_udpListener);
 }
 
@@ -24,5 +24,10 @@ void ServerGame::run()
 void ServerGame::joinGame(const std::weak_ptr<Network::AListenSocket>& that,
                           const std::shared_ptr<Network::Identity>& id, const Network::Buffer& data)
 {
+  auto listener = that.lock();
+  std::shared_ptr<ClientGame> cg(new ClientGame(id, that));
+  cg->readData(data);
 
+  _clients.push_back(cg);
+  std::cout << "New gameClient: " << id->ip << ":" << id->port << std::endl;
 }
