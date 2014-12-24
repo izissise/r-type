e812921ba3665073b1995849de0882b31171b35f
Hugues Morisset <morisset.hugues@gmail.com>
2014-12-24 01:42:25 +0100

    Link against dl on unix, and commented with what it should be

--- common/inc/Unix/DLLoaderUnix.hpp
+++ common/inc/Unix/DLLoaderUnix.hpp
@@ -1,10 +1,9 @@
-#ifdef __unix__
-
-# ifndef	DLLOADERUNIX_HPP_
+#ifndef	DLLOADERUNIX_HPP_
 # define	DLLOADERUNIX_HPP_
 
-# include	"IDLLoader.hpp"
 # include	<dlfcn.h>
+
+# include	"IDLLoader.hpp"
 
 template<typename T>
 class DLLoader //: public IDLLoader<T>
@@ -45,5 +44,73 @@ public:
 protected:
 };
 
-# endif // !DLLOADERUNIX_HPP_
+
+/*
+template<typename T>
+DLLoader<T>::DLLoader(const std::string& lib)
+{
+  _libname = std::string("./") + lib;
+  _handle = NULL;
+  _loaded = false;
+  _symbolname = "instance";
+}
+
+template<typename T>
+DLLoader<T>::~DLLoader()
+{
+  char* err;
+
+  if (_loaded)
+    if (dlclose(_handle))
+      {
+        err = dlerror();
+        throw nFault(std::string("Can't close library: ") + err, false);
+      }
+}
+
+template<typename T>
+T DLLoader<T>::getInstance()
+{
+  void* sym;
+  T ret;
+  T (*func)();
+
+  sym = loadSym(_symbolname);
+  func = reinterpret_cast<T (*)()>(sym);
+  ret = NULL;
+  if (func != NULL)
+    ret = func();
+  return (ret);
+}
+
+template<typename T>
+void DLLoader<T>::loadLib()
+{
+  char* err;
+
+  _handle = dlopen(_libname.c_str(), RTLD_LAZY);
+  if (_handle == NULL)
+    {
+      err = dlerror();
+      throw nFault(std::string("Can't load library: ") + err, false);
+    }
+  _loaded = true;
+}
+
+template<typename T>
+void* DLLoader<T>::loadSym(const std::string& symbolname)
+{
+  void* sym;
+  char* err;
+
+  if (!_loaded)
+    loadLib();
+  dlerror();
+  sym = dlsym(_handle, symbolname.c_str());
+  if (sym == NULL && (err = dlerror()) == NULL)
+    throw nFault(std::string("Can't get symbol: ") + err, false);
+  return (sym);
+}
+*/
+
 #endif
