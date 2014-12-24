c2032cd608bdace85b80d900f72343dc1fd4c843
cisner_d <cisner_d@git.epitech.eu>
2014-12-24 17:14:59 +0100

    dirhandler unix up to date

--- common/src/Unix/DirHandlerUnix.cpp
+++ common/src/Unix/DirHandlerUnix.cpp
@@ -1,8 +1,12 @@
-#include "Unix/DirHandlerUnix.hpp"
+#ifdef __unix__
+# include	"DirHandlerUnix.hh"
+# include	<stdio.h>
+# include	<sys/types.h>
+# include	<dirent.h>
 
 DirHandler	*DirHandler::instance = NULL;
 
-DirHandler *DirHandler::GetInstance()
+DirHandler				*DirHandler::GetInstance()
 {
 	if (instance == NULL)
 		instance = new DirHandler();
@@ -12,13 +16,52 @@ DirHandler *DirHandler::GetInstance()
 std::list<std::string>	DirHandler::GetFilesFromDir(const std::string &dir) const
 {
 	std::list<std::string>	list;
+	DIR						*dfd;
+	struct dirent			*file;
 
+	dfd = opendir(dir);
+	if (dfd == NULL)
+	{
+		std::cerr << "error can't open dir : " << dir << std::endl;
+	}
+	else
+	{
+		while ((file = readdir(dfd)) != NULL)
+		{
+			std::string	name = file->d_name;
+
+			std::cout << "FILE = " << name << std::endl;
+			list.push_back(name);
+		}
+		closedir(dfd);
+	}
 	return (list);
 }
 
 std::list<std::string>	DirHandler::GetLibsFromDir(const std::string &dir) const
 {
 	std::list<std::string>	list;
+	DIR						*dfd;
+	struct dirent			*file;
 
+	dfd = opendir(dir);
+	if (dfd == NULL)
+	{
+		std::cerr << "error can't open dir : " << dir << std::endl;
+	}
+	else
+	{
+		while ((file = readdir(dfd)) != NULL)
+		{
+			std::string	name = file->d_name;
+
+			std::cout << "FILE = " << name << std::endl;
+			if (name.find(".lib") != std::string::npos)
+				list.push_back(name);
+		}
+		closedir(dfd);
+	}
 	return (list);
 }
+
+#endif // __unix__
